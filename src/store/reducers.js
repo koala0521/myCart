2c2b1cb1e4dab3c1b3347ba2c426d2382a193c27
unknown <gaopengfei@mvwchina.com>
2018-02-22 14:33:25 +0800

    应用的reducers

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,11 +1,13 @@
 import {  combineReducers } from 'redux'; //合并子reducer
 import { routerReducer } from 'react-router-redux'; // routerReducer 函数 , redux监听保存 router的变化
 
+import {category , goodsDetail } from 'route/Shop';
+import { cart } from 'route/Cart';
 
+// 应用的reducers，由多个子reducer构成
 export default combineReducers({
-    router:routerReducer
-    // ,
-    // category,
-    // goodsDetail,
-    // cart
+    router:routerReducer,
+    category,
+    goodsDetail,
+    cart
 });
